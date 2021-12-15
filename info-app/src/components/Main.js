0afca439b5267212064e9bf30ebf3146dc832213
Oswaldo Rodriguez <Owner2303@gmail.com>
2021-12-15 15:01:36 -0500

    styling info paging nav bar

---
+++ info-app/src/components/Main.js
@@ -0,0 +1,7 @@
+export default function Main(){
+    return(
+        <div>
+            <h1>Hello from the Main!</h1>
+        </div>
+    )
+}
---
+++ info-app/src/components/NavBar.js
@@ -0,0 +1,14 @@
+export default function Nav(){
+    return(
+        <div className='navBar'>
+            <div className="imageContainer">
+                <img className="image" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K" alt=""/>
+                <h3>ReactFacts</h3>
+            </div>
+            <div className="titleBar">
+                <h3>React Course - Project 1</h3>
+
+            </div>
+        </div>
+    )
+}
