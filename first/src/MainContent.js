e634210cac399c945568d42e4e271cae87c71c42
Oswaldo Rodriguez <Owner2303@gmail.com>
2021-12-14 23:37:29 -0500

    composing the file

---
+++ first/src/MainContent.js
@@ -0,0 +1,12 @@
+export default function MainContent(){
+    return(
+        <div className="mainContent">
+            <h2> Why is Own learning React</h2>
+            <ol>
+                <li>Cause I need it for a job</li>
+                <li>Cause I don't know many JS libraries</li>
+                <li>because the market is hot!!</li>
+            </ol>
+        </div>
+    );
+}
--- first/src/Why.js
+++ first/src/Why.js
@@ -1,66 +1,6 @@
-function Header(){
-    return(
-        <header>
-            <nav className="navigation">
-                <div className="imageContainer">
-                    <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K" className="image"  />
-                </div>
-                <ul>
-                    <li className="list active">
-                        <a href="#">
-                            <span className="icon"><ion-icon name="planet-outline"></ion-icon></span>
-                            <span className="text">Home</span>
-                        </a>
-                    </li>
-                    <li className="list">
-                        <a href="#">
-                            <span className="icon"><ion-icon name="person-circle-outline"></ion-icon></span>
-                            <span className="text">Profile</span>
-                        </a>
-                    </li>
-                    <li className="list">
-                        <a href="#">
-                            <span className="icon"><ion-icon name="logo-snapchat"></ion-icon></span>
-                            <span className="text">Message</span>
-                        </a>
-                    </li>
-                    <li className="list">
-                        <a href="#">
-                            <span className="icon"><ion-icon name="camera-outline"></ion-icon></span>
-                            <span className="text">Photos</span>
-                        </a>
-                    </li>
-                    <li className="list">
-                        <a href="#">
-                            <span className="icon"><ion-icon name="cog-outline"></ion-icon></span>
-                            <span className="text">Settings</span>
-                        </a>
-                    </li>
-                    <div className="indicator"></div>
-                </ul>
-            </nav>
-        </header>
-    );
-}
-function Footer(){
-    return(
-        <footer className="footer">
-            <small>© 2021 Rodríguez development. All rights reserved.</small>
-        </footer>
-    )
-}
-function MainContent(){
-    return(
-        <div className="mainContent">
-            <h2> Why is Own learning React</h2>
-            <ol>
-                <li>Cause I need it for a job</li>
-                <li>Cause I don't know many JS libraries</li>
-                <li>because the market is hot!!</li>
-            </ol>
-        </div>
-    );
-}
+import Header from "./Header";
+import MainContent from "./MainContent";
+import Footer from "./Footer";
 
 function YouGotItOwn(){
     return(
