071e3238eac87d44689e6910c8f3f2e8ac894f97
Oswaldo Rodriguez <Owner2303@gmail.com>
2021-12-18 17:10:40 -0500

    info page built

--- solo-project/src/components/Info.js
+++ solo-project/src/components/Info.js
@@ -1,5 +1,20 @@
-export default function Info(){
-    return(
-        <h3>This one comes from the Info</h3>
+import Own from './Own.jpg';
+
+export default function Info() {
+    return (
+        <section>
+            <div className="InfoCard">
+                <img src={Own} alt="Own's photo"></img>
+                <h3>Own Rodriguez</h3>
+                <p className="CareerName">Full-Stack Developer</p>
+                <a href="#">oswaldorodriguez.website</a>
+
+                <div className="Buttons">
+                    <button><span></span>Email</button>
+                    <button><span></span>LinkedIn</button>
+                </div>
+            </div>
+
+        </section>
     );
 }
