61537bd049115c3e6c0083fd83a833bd26b65889
Oswaldo Rodriguez <Owner2303@gmail.com>
2021-12-22 18:19:37 -0500

    message

--- props/src/component/Joke.js
+++ props/src/component/Joke.js
@@ -2,7 +2,7 @@ export default function Joke(props){
     return(
         <div>
             <h1>{props.setup}</h1>
-            <h3>{props.punchline}</h3>
+            <h3>{props.punchline}</h3><hr/>
         </div>
 
     )
