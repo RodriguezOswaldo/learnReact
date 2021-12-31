f07ad9e043e60a172e6659b5942e13e6915cd607
Oswaldo Rodriguez <Owner2303@gmail.com>
2021-12-31 02:33:56 -0500

    memes

--- meme/src/components/Meme.js
+++ meme/src/components/Meme.js
@@ -1,19 +1,35 @@
+import memesData from "../memesData";
+
 export default function Meme() {
+
+    let url
+
+    function getMemeImage(){
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        url = memesArray[randomNumber].url
+        console.log(url)
+    }
     return (
         <main>
-            <form className="form">
+            <div className="form">
                 <input
                     type="text"
                     placeholder="Top text"
-                    className="form--input"/>
+                    className="form--input"
+                />
                 <input
                     type="text"
                     placeholder="Bottom text"
-                    className="form--input"/>
+                    className="form--input"
+                />
                 <button
                     className="form--button"
-                >Get a new meme image 🖼</button>
-            </form>
+                    onClick={getMemeImage}
+                >
+                    Get a new meme image 🖼
+                </button>
+            </div>
         </main>
             )
             }
