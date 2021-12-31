ef7de7c950cfb4e4bf8b7d460e3678e7429ff67a
Oswaldo Rodriguez <Owner2303@gmail.com>
2021-12-31 00:41:52 -0500

    meme generator nav bar

---
+++ meme/src/components/Nav.js
@@ -0,0 +1,14 @@
+export default function Nav() {
+    return (
+        <nav className="Nav">
+            <div className="leftNav">
+                <img src="trollFace.png" className="logo"/>
+                <p> Meme Generator</p>
+            </div>
+            <div className="rightNav">
+                <p className="navTwo">React Course - Project 3</p>
+            </div>
+
+        </nav>
+    )
+}
