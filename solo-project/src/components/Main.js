39404d94482296fe233f9a436f91553046654e93
Oswaldo Rodriguez <Owner2303@gmail.com>
2023-05-27 13:51:48 -0400

    meme generator

--- solo-project/src/components/Main.js
+++ solo-project/src/components/Main.js
@@ -1,7 +1,5 @@
 import Info from "./Info";
-import About from "./About";
-import Footer from "./Footer";
-import Interest from "./Interest";
+
 export default function Main(){
     return(
         <div>
