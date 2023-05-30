74c93a5cd2f9bce21edafad584477cd06135b9cc
Oswaldo Rodriguez <Owner2303@gmail.com>
2023-05-29 21:04:42 -0400

    added some styles to the existing meme project

---
+++ meme/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img className="header--image" src="https://upload.wikimedia.org/wikipedia/en/9/9a/Trollface_non-free.png" />
+            <h2 className="header--title">Header component</h2>
+            <h4 className="header--project">React Course - Project 3</h4>
+        </header>
+    )
+}
