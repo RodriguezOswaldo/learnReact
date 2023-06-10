503e2bd447d6cef1aac96a16398987116b76c021
Oswaldo Rodriguez <Owner2303@gmail.com>
2023-06-10 13:05:43 -0400

    updating size

--- meme/src/components/Header.js
+++ meme/src/components/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <header className="header">
             <img className="header--image" src="https://upload.wikimedia.org/wikipedia/en/9/9a/Trollface_non-free.png" />
-            <h2 className="header--title">Header component</h2>
+            <h2 className="header--title">Meme Generator</h2>
             <h4 className="header--project">React Course - Project 3</h4>
         </header>
     )
