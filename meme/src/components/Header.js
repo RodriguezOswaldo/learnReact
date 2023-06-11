97cdefcd151c3e0ec378faf88c0e9983293d1872
Oswaldo Rodriguez <Owner2303@gmail.com>
2023-06-11 16:11:23 -0400

    form design

--- meme/src/components/Header.js
+++ meme/src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Header() {
     return (
         <header className="header">
-            <img className="header--image" alt="" tsrc="https://upload.wikimedia.org/wikipedia/en/9/9a/Trollface_non-free.png" />
+            <img className="header--image" alt="" src="https://m.media-amazon.com/images/I/61fY3YrexrL._AC_UF894,1000_QL80_.jpg" />
             <h2 className="header--title">Meme Generator</h2>
             <h4 className="header--project">React Course - Project 3</h4>
         </header>
--- meme/src/components/Meme.js
+++ meme/src/components/Meme.js
@@ -4,9 +4,11 @@ export default function Meme() {
     return (
         <main>
             <form className="form">
-                <input type="text" />
-                <input type="text" />
-                <button>Get a new meme image 🖼</button>
+                <input placeholder="Top Text" type="text"
+                    className="form--input" />
+                <input placeholder="Bottom Text" type="text"
+                    className="form--input" />
+                <button className="form--button"> Get a new meme image 🖼</button>
             </form>
         </main>
     )
