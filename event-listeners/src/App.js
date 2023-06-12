b91ac5fe2013a30bb7d3f085f6762cb361ce030c
Oswaldo Rodriguez <Owner2303@gmail.com>
2023-06-12 11:52:40 -0400

    done with react mouse events

--- event-listeners/src/App.js
+++ event-listeners/src/App.js
@@ -2,22 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  function handleClick() {
+    console.log("I was clicked!")
+  }
+  function handleOnMouseOver() {
+    console.log("mouseOver")
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <img onMouseOver={handleOnMouseOver} alt="random 200px large" src="https://picsum.photos/640/360" />
+      <button onClick={handleClick}>Click me</button>
     </div>
   );
 }
